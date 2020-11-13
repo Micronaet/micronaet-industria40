cd231d82dbad2efaf956796270e89e39b38c04fc
nicothebrush <nicola.riolini@gmail.com>
2020-11-13 21:44:13 +0100

    generic bugfix (fast message)

--- industria40_base/agent/alarms/robot1_bot.py
+++ industria40_base/agent/alarms/robot1_bot.py
@@ -73,7 +73,6 @@ bot.sendMessage(
     '[INFO]: %s: Attivazione procedure controllo allarmi' % robot_name,
 )
 print('Start alarm procedure master loop')
-pdb.set_trace()
 while True:  # Master loop:
     # A. Mount server:
     print('Try to mount robot server')
