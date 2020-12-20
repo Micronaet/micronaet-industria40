a887d945feec5585bbb8761106722c99ef124051
nicothebrush <nicola.riolini@gmail.com>
2020-12-20 13:40:58 +0100

    add pool procedure (to be completed)

---
+++ industria40_base/agent/opcua_poll/robot_job_polling.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# ODOO (ex OpenERP)
+# Open Source Management Solution
+# Copyright (C) 2001-2015 Micronaet S.r.l. (<http://www.micronaet.it>)
+# Developer: Nicola Riolini @thebrush (<https://it.linkedin.com/in/thebrush>)
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+import os
+import erppeek
+import ConfigParser
+import opcua
+import pdb
+
+def host_up(host):
+    if os.system('ping -c 2 %s' % host) is 0:
+        return True
+    else:
+        return False
+
+# -----------------------------------------------------------------------------
+# Read configuration parameter:
+# -----------------------------------------------------------------------------
+# From config file:
+cfg_file = os.path.expanduser('../openerp.cfg')
+
+config = ConfigParser.ConfigParser()
+config.read([cfg_file])
+dbname = config.get('dbaccess', 'dbname')
+user = config.get('dbaccess', 'user')
+pwd = config.get('dbaccess', 'pwd')
+server = config.get('dbaccess', 'server')
+port = config.get('dbaccess', 'port')   # verify if it's necessary: getint
+
+# -----------------------------------------------------------------------------
+# Connect to ODOO:
+# -----------------------------------------------------------------------------
+odoo = erppeek.Client(
+    'http://%s:%s' % (
+        server, port),
+    db=dbname,
+    user=user,
+    password=pwd,
+    )
+
+job_pool = odoo.model('industria.job')
+
+job_ids = job_pool.search([
+    ('database_id.mode', '=', 'opcua'),  # Only opcua database job
+    ('state', '=', 'RUNNING'),  # Only running job
+])
+
+# TODO Test OPCUA variables
+# TODO Update ODOO if done (add time, duration)
