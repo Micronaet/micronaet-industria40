7c8072354b85aaacee429700a485dff063e6b4dd
nicothebrush <nicola.riolini@gmail.com>
2020-12-21 11:47:53 +0100

    generic bugfix (fast message)

--- industria40_base/agent/opcua_poll/robot_job_polling.py
+++ industria40_base/agent/opcua_poll/robot_job_polling.py
@@ -59,7 +59,7 @@ def set_data_value(robot, node_description, value):
     node = robot.get_node(node_description)
     pdb.set_trace()
     try:
-        node.set_value(value)  # set_value
+        node.set_data_value(value, varianttype=1)  # set_value
     except:
         print('Cannot read, robot unplugged?\n%s' % (sys.exc_info(), ))
         return False
@@ -132,8 +132,8 @@ mask = 'ns=3;s="DB_1_SCAMBIO_DATI_I4_0"."%s"[%s]'
 
 set_data_value(
     robot,
-    mask % ('Velocità', 1),
-    210.0,
+    mask % ('Temperatura', 1),
+    210,
 )
 
 pdb.set_trace()
