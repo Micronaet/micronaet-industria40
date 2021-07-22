b19186616dc72feb947a1508af3c1c2b008296da
nicothebrush <nicola.riolini@gmail.com>
2021-07-22 21:17:47 +0200

    generic bugfix (fast message)

---
+++ industria40_base/cron/tagliatubi/alarm_check.py
@@ -0,0 +1,162 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# ODOO (ex OpenERP)
+# Open Source Management Solution
+# Copyright (C) 2001-2015 Micronaet S.r.l. (<https://micronaet.com>)
+# Developer: Nicola Riolini @thebrush (<https://it.linkedin.com/in/thebrush>)
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+import os
+import sys
+import pdb
+import time
+import telepot
+import ConfigParser
+import pickle
+from datetime import datetime
+
+# -----------------------------------------------------------------------------
+# Read configuration parameter:
+# -----------------------------------------------------------------------------
+import pdb; pdb.set_trace()
+pickle_file = os.path.expanduser('./error.status.pickle')
+cfg_file = os.path.expanduser('./robot.cfg')
+config = ConfigParser.ConfigParser()
+config.read([cfg_file])
+
+# -----------------------------------------------------------------------------
+# Utility:
+# -----------------------------------------------------------------------------
+def clean(value):
+    value = value or ''
+    res = ''
+    for c in value:
+        if ord(c) > 127:
+            res += '#'
+        else:
+            res += c
+    return res
+
+
+# -----------------------------------------------------------------------------
+# Load error comment:
+# -----------------------------------------------------------------------------
+# Main list:
+try:
+    file_lines = pickle.load(open(pickle_file, 'rb'))
+except:
+    file_lines = {}
+
+# -----------------------------------------------------------------------------
+# Parameters:
+# -----------------------------------------------------------------------------
+# File:
+path_robot = config.get('File', 'robot')
+file_check = config.get('File', 'check')
+
+# Telegram:
+telegram_token = config.get('Telegram', 'token')
+telegram_group = config.get('Telegram', 'group')
+telegram_limit = 10
+
+# Detail:
+robot_name = config.get('Detail', 'name')
+
+# -----------------------------------------------------------------------------
+# Connect to Telegram:
+# -----------------------------------------------------------------------------
+bot = telepot.Bot(telegram_token)
+bot.getMe()
+bot.sendMessage(
+    telegram_group,
+    '[INFO]: %s: Attivazione procedure controllo allarmi' % robot_name,
+)
+
+print('Start alarm procedure master loop')
+while True:  # Master loop:
+    # -------------------------------------------------------------------------
+    # Check file test:
+    # -------------------------------------------------------------------------
+    while not os.path.isfile(file_check):
+        if not os.path.isfile(file_check):
+            time.sleep(2 * 60)
+
+    try:
+        bot.sendMessage(
+            telegram_group,
+            '%s\n[INFO]: Connesso con il Robot: %s' % (
+                '-' * 40,
+                robot_name,
+            )
+        )
+    except:
+        print('Error telegram message: Robot %s connect' % robot_name)
+
+    # -------------------------------------------------------------------------
+    # Check alarm loop:
+    # -------------------------------------------------------------------------
+    while True:
+        try:
+            filename = datetime.now().strftime('Alarms_%y_%m_%d.txt')
+            if filename not in file_lines:
+                file_lines[filename] = 0
+            fullname = os.path.join(path_robot, filename)
+            if not os.path.isfile(fullname):
+                print('Nessun file di errore: %s' % fullname)
+                continue
+            last_total = file_lines[filename]
+            row = error_counter = 0
+            print('Check alarm')
+            for line in open(fullname, 'r'):
+                row += 1
+                if line > last_total:
+                    continue
+                event_text = \
+                    'Robot: %s\nMessaggio: %s' % (
+                        robot_name,
+                        line,
+                        )
+
+                # Telegram message limit prevent:
+                if error_counter > telegram_limit:
+                    error_counter = 0
+                    print('Telegram wait period...')
+                    time.sleep(15)  # wait 15 seconds
+                else:
+                    error_counter += 1
+                event_text = clean(event_text)
+                bot.sendMessage(telegram_group, event_text)
+
+                time.sleep(30)  # Master period for check error
+        except:
+            print('Error in file access')
+            break
+
+    try:
+        bot.sendMessage(
+            telegram_group,
+            '[WARNING]: Disconnesso dal Robot (spento o bloccato il programma '
+            'di controllo?): %s\n%s' % (
+                '-' * 40,
+                robot_name,
+            ))
+    except Exception:
+        print Exception
+        print('Telegram error: Robot shutdown?')
+
+pickle.dump(file_lines, load(open(pickle_file, 'wb')))
--- xml_management/xml_management.py
+++ xml_management/xml_management.py
@@ -21,50 +21,48 @@
 #
 ###############################################################################
 
-import os
 import pdb
-import sys
 import logging
 from openerp.osv import fields, osv, expression, orm
 import xml.etree.cElementTree as ElementTree
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
-from openerp import SUPERUSER_ID, api
 from openerp.tools.translate import _
 
 
 _logger = logging.getLogger(__name__)
 
+
 class XmlDictConfig(dict):
-    '''
-    Note: need to add a root into if no exising
+    """
+    Note: need to add a root into if no existing
     Example usage:
-    >>> tree = ElementTree.parse('your_file.xml')
-    >>> root = tree.getroot()
-    >>> xmldict = XmlDictConfig(root)
+    >> tree = ElementTree.parse('your_file.xml')
+    >> root = tree.getroot()
+    >> xmldict = XmlDictConfig(root)
     Or, if you want to use an XML string:
-    >>> root = ElementTree.XML(xml_string)
-    >>> xmldict = XmlDictConfig(root)
+    >> root = ElementTree.XML(xml_string)
+    >> xmldict = XmlDictConfig(root)
     And then use xmldict for what it is... a dict.
-    '''
+    """
     def __init__(self, parent_element):
         if parent_element.items():
-            self.updateShim( dict(parent_element.items()) )
+            self.updateShim( dict(parent_element.items()))
         for element in parent_element:
             if len(element):
                 aDict = XmlDictConfig(element)
-            #   if element.items():
-            #   aDict.updateShim(dict(element.items()))
+                # if element.items():
+                # aDict.updateShim(dict(element.items()))
                 self.updateShim({element.tag: aDict})
-            elif element.items():    # items() is specialy for attribtes
-                elementattrib= element.items()
+            elif element.items():    # items() is specially for attributes
+                elementattrib = element.items()
                 if element.text:
-                    elementattrib.append((element.tag,element.text ))     # add tag:text if there exist
+                    elementattrib.append((element.tag, element.text))     # add tag:text if there exist
                 self.updateShim({element.tag: dict(elementattrib)})
             else:
                 self.updateShim({element.tag: element.text})
 
-    def updateShim (self, aDict ):
+    def updateShim (self, aDict):
         for key in aDict.keys():   # keys() includes tag and attributes
             if key in self:
                 value = self.pop(key)
@@ -77,69 +75,7 @@ class XmlDictConfig(dict):
                     value.append(aDict[key])
                     self.update({key: value})
             else:
-                self.update({key:aDict[key]})  # it was self.update(aDict)
-class XmlDictConfig(dict):
-    def get_in_type(self, text):
-        """ Return in correct value
-        """
-        # Boolean:
-        if text == 'true':
-            return True
-        if text == 'false':
-            return False
-
-            # Datetime
-
-        # Date
-
-        # Integer
-        if not (text or '').startswith('0'):
-            try:
-                return int(text)
-            except:
-                pass
-
-                # Float
-            try:
-                return float(text)
-            except:
-                pass
-        return text
-
-    def __init__(self, parent_element):
-        """ Start instance for this object
-        """
-        if parent_element.items():
-            self.update(dict(parent_element.items()))
-        for element in parent_element:
-            if element:
-                # treat like dict - we assume that if the first two tags
-                # in a series are different, then they are all different.
-                if len(element) == 1 or element[0].tag != element[1].tag:
-                    aDict = XmlDictConfig(element)
-                # treat like list - we assume that if the first two tags
-                # in a series are the same, then the rest are the same.
-                else:
-                    # here, we put the list in dictionary; the key is the
-                    # tag name the list elements all share in common, and
-                    # the value is the list itself
-                    # todo manage this error:
-                    pdb.set_trace()
-                    aDict = {element[0].tag: XmlListConfig(element)}
-                # if the tag has attributes, add those to the dict
-                if element.items():
-                    aDict.update(dict(element.items()))
-                self.update({element.tag: aDict})
-            # this assumes that if you've got an attribute in a tag,
-            # you won't be having any text. This may or may not be a
-            # good idea -- time will tell. It works for the way we are
-            # currently doing XML configuration files...
-            elif element.items():
-                self.update({element.tag: dict(element.items())})
-            # finally, if there are no child tags and no attributes, extract
-            # the text
-            else:
-                self.update({element.tag: self.get_in_type(element.text)})
+                self.update({key: aDict[key]})  # it was self.update(aDict)
 
 
 class XMLManagement(orm.Model):
