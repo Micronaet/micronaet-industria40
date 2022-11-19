4bd771f8833fd34f47918c8cb980926249be186c
nicothebrush <nicola.riolini@gmail.com>
2022-11-19 15:44:46 +0100

    generic bugfix (fast message)

---
+++ Agent/flask_json_agent/client_test.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+###############################################################################
+# Copyright (C) 2001-2015 Micronaet S.r.l. (<https://micronaet.com>)
+# Developer: Nicola Riolini @thebrush
+#            https://it.linkedin.com/in/thebrush
+#            https://linktr.ee/nicolariolini
+###############################################################################
+
+import os
+import pdb
+import requests
+import json
+
+# -----------------------------------------------------------------------------
+# Authenticate to get Session ID:
+# -----------------------------------------------------------------------------
+url = 'http://localhost:5000/API/v1.0/micronaet/laucher'
+headers = {
+    'content-type': 'application/json',
+}
+payload = {
+    'jsonrpc': '2.0',
+    'params': {
+        'command': 'statistic',
+        'parameters': {
+            'mysql': {
+                'host': '192.168.1.1',
+                'port': '3306',
+                'user': 'user',
+                'password': '',
+                'database': '',
+                'use_pure': False,
+                },
+            },
+        },
+    }
+
+response = requests.post(url, headers=headers, data=json.dumps(payload))
+response_json = response.json()
+if response_json['success']:
+    print(response_json)
