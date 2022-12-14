3e7553b14ce5247536c8f4c1afb6d1e502836d3a
nicothebrush <nicola.riolini@gmail.com>
2022-12-14 09:51:38 +0100

    generic bugfix (fast message)

--- Agent/flask_json_agent/app.py
+++ Agent/flask_json_agent/app.py
@@ -98,7 +98,7 @@ def ODOOCall():
         'success': False,
         'reply': {},
         }
-
+    pdb.set_trace()
     if 'mysql' in params:
         # MySQL call:
         mysql_param = params.get('mysql', {})
