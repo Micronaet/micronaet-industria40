519c24447114cebaaca8536e4103b77d2f5bf471
nicothebrush <nicola.riolini@gmail.com>
2023-04-03 15:58:54 +0200

    generic bugfix (fast message)

--- MES/webserver.py
+++ MES/webserver.py
@@ -78,7 +78,8 @@ def parser_write_hour(value):
         return ''
 
     gmt_gap = 2  # 2 legal hour, 1 solar hour
-    dt = value  # datetime.strptime(value, '%Y-%m-%d %H:%M:%S')
+    pdb.set_trace()
+    dt = datetime.strptime(value, '%Y-%m-%d %H:%M:%S')
     italian_dt = dt + timedelta(hours=gmt_gap)
     return italian_dt.strftime('%d-%m-%Y %H:%M:%S')
 
